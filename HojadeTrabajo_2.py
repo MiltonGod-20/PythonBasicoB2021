3825505dfcffa0b3e972e2f15970c68a96a5b4bc
Million Dalas <godinez.202004782@gmail.com>
2021-02-12 17:38:11 -0600

    Hoja de Trabajo No. 2
    Python Basico B - HT2

---
+++ HojadeTrabajo_2.py
@@ -0,0 +1,30 @@
+# EJERCICIO 1
+clave_usuario = "contraseña"
+Llave = input("INTRODUZCA LA LLAVE: ")
+if clave_usuario == Llave.lower():
+    print("ACCESO CONCEDIO, LA LLAVE COINCIDE")
+else :
+        print("CONTRASEÑA INCORRECTA, LA LLAVE NO COINCIDE")
+
+
+
+# EJERCICIO 2
+nombre = input("INTRODUCE TU NOMBRE: ")
+genero = input("INTRODUCE TU SEXO (MASC. O FEM)")
+
+if (genero == "FEM" and nombre.lower() < 'm'):
+            input("ERES DEL GRUPO A")
+else :
+            input("ERES DEL GRUPO B")
+if (genero == "MASC" and nombre.lower() > 'n'):
+            input("ERES DEL GRUPO A")
+else:
+            input("ERES DEL GRUPO B")
+
+
+         
+
+
+
+
+
